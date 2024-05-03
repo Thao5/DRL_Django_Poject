3734137e6bf381caae2b948177d0ca30dd4869e6
quocthao9899@gmail.com <quocthao9899@gmail.com>
2024-05-04 02:56:49 +0700

    update

--- SystemDRL/DRLProj/DRL/views.py
+++ SystemDRL/DRLProj/DRL/views.py
@@ -17,6 +17,12 @@ from .paginator import DRLPaginator
 from .perms import HasGroupTLSVPermission
 from django.templatetags.static import static
 from django.conf import settings
+from django.http import FileResponse
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+
 
 # Create your views here.
 
@@ -241,3 +247,31 @@ class ThanhTichViewSet(viewsets.ViewSet, generics.ListAPIView):
     queryset = ThanhTichNgoaiKhoa.objects.all()
     serializer_class = ThanhTichNgoaiKhoaSerializer
     pagination_class = DRLPaginator
+
+    @action(methods=['GET'], url_path="statspdf", detail=False)
+    def StatsPDF(self, request):
+        buf = io.BytesIO()
+        c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+        textob = c.beginText()
+        textob.setTextOrigin(inch, inch)
+        textob.setFont("Helvetica", 14)
+
+        lines = [
+            "this is line 1",
+            "this is line 2",
+            "this is line 3",
+            "this is line 4",
+            "this is line 5",
+        ]
+
+        for l in lines:
+            textob.textLine(l)
+
+        c.drawText(textob)
+        c.showPage()
+        c.save()
+        buf.seek(0)
+        return FileResponse(buf, as_attachment=True, filename="statsPDF.pdf")
+
+
+
